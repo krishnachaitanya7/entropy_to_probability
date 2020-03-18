06883797d035cb598f7772c22ed16e118209e0a7
Krishna <chaitanyaradon89@gmail.com>
2020-03-17 22:30:12 -0600

    Some intermediate progress

--- entropy_to_probability.py
+++ entropy_to_probability.py
@@ -6,26 +6,78 @@ from tensorflow.keras import layers
 from sklearn.metrics import mean_squared_error
 import numpy as np
 import datetime
+import shutil
+import os
 
-changed_lr = False
+global model
+# changed_lr = False
 folder_name = "my_model"
+
+
+def save_model(fl_name):
+    global model
+    if os.path.isdir(fl_name):
+        shutil.rmtree(fl_name)
+        os.mkdir(fl_name)
+    model.save(model, save_format='tf')
+
+
+class my_model(tf.keras.Model):
+    def __init__(self):
+        super(my_model, self).__init__()
+        self.l1(layers.SimpleRNN(5, return_sequences=True))
+        self.l2(layers.SimpleRNN(5))
+        self.l3(layers.Dense(1, activation=tf.keras.activations.sigmoid))
+
+    def call(self, inputs):
+        # https://www.tensorflow.org/api_docs/python/tf/keras/Model
+        # https://www.tensorflow.org/guide/keras/custom_layers_and_models
+        # You gotta overwrite this call function so that you can implement the forward pass
+        x = self.l1(inputs)
+        x = self.l2(x)
+        x = self.l3(x)
+        return x
+
+
+class model_saver(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        save_after_epochs = 100  # We will be saving after every these many no of epochs
+        if epoch % save_after_epochs == 0:
+            save_model(folder_name)
+
 
 class LearningRateReducerCb(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
-        global changed_lr
-        if not changed_lr and logs['mae'] < 0.0009:
-            old_lr = self.model.optimizer.lr.read_value()
-            new_lr = old_lr / 10
-            print("\nEpoch: {}. Reducing Learning Rate from {} to {}".format(epoch, old_lr, new_lr))
-            self.model.optimizer.lr.assign(new_lr)
-            changed_lr = True
+        if logs['loss'] < 1e-04:
+            model.stop_training = True
+
+
+def make_model(fl_name):
+    global model
+    # First find if there is already a saved model from which you can load on
+    if len(os.listdir(fl_name)) != 0:
+        # Means we have a saved model, lets load it boys
+        print("Saved Checkpoints found, loading it...")
+        model = tf.keras.models.load_model(fl_name)
+    else:
+        # Model ain't present, you have to do it from start
+        print("No checkpoints found. Loading from scratch...")
+        # model = keras.Sequential()
+        # model.add(layers.SimpleRNN(5, return_sequences=True))
+        # model.add(layers.SimpleRNN(5))
+        # model.add(layers.Dense(1, activation=tf.keras.activations.sigmoid))
+        # model.compile(loss=tf.keras.losses.MeanAbsoluteError(),
+        #               optimizer=tf.keras.optimizers.Nadam(1e-4),
+        #               metrics=['mae'])
+        model = my_model()
+
 
 if __name__ == "__main__":
     df = pd.read_csv("entropy.csv")
     X = df["Input"].values
     y = df["Output"].values
     file_name = "Final_values.csv"
-    MAX_EPOCHS = 2000
+    MAX_EPOCHS = 500
     BATCH = 100
     X_train, X_testing, y_train, y_testing = train_test_split(X, y, test_size=0.2)
     X_test, X_val, y_test, y_val = train_test_split(X_testing, y_testing, test_size=0.5)
@@ -38,15 +90,9 @@ if __name__ == "__main__":
     y_val = y_val.reshape((len(y_val), 1, 1))
     log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-    model = keras.Sequential()
-    model.add(layers.SimpleRNN(5, return_sequences=True))
-    model.add(layers.SimpleRNN(5))
-    model.add(layers.Dense(1, activation=tf.keras.activations.sigmoid))
-    model.compile(loss=tf.keras.losses.MeanAbsoluteError(),
-                  optimizer=tf.keras.optimizers.Nadam(1e-4),
-                  metrics=['mae'])
+    make_model(folder_name)
     model.fit(X_train, y_train, epochs=MAX_EPOCHS, batch_size=BATCH, verbose=1,
-              callbacks=[LearningRateReducerCb(), tensorboard_callback],
+              callbacks=[LearningRateReducerCb(), tensorboard_callback, model_saver()],
               validation_data=(X_test, y_test))
     y_pred = model.predict(X_val)
     rmse_calculated = mean_squared_error(y_pred.flatten(), y_val.flatten())
@@ -56,5 +102,5 @@ if __name__ == "__main__":
     np.savetxt(file_name, final_save_matrix,
                delimiter=',', header="X_validation,Original_Y,Predicted_y", comments="")
     # os.remove(file_name)
-    model.save(folder_name, save_format='tf')
+    save_model(folder_name)
     # Noted RMSE: 4.1091989895786396e-06
